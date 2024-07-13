5968f0f5911fd9fd93bd232e35fd14894b39d3c5
KeachT <503.b03@gmail.com>
2024-07-13 14:39:20 +0900

    refactor(generate_horse_combinations): improve user input handling and code structure

--- generate_horse_combinations.rb
+++ generate_horse_combinations.rb
@@ -1,15 +1,18 @@
-def main
+def get_unique_names
   puts "\nPlease enter names separated by commas (ex. Glass, Special, El)"
-  input_names = gets.chomp
-  unique_names = input_names.split(",").uniq
+  input_names = gets.chomp.split(",")
+  input_names.map(&:strip).uniq
+end
 
+def print_combinations(names)
   puts "\nCombinations are as follows (Combination of 2 names, without duplication, unordered):"
-
-  unique_names.combination(2).each_with_index do |name, index|
-    puts "#{index + 1} : #{name.join(",")}"
-  end
+  names_combination = names.combination(2)
+  names_combination.each_with_index { |name, index| puts "#{index + 1} : #{name.join(", ")}" }
 end
 
-if __FILE__ == $0
-  main
+def main
+  unique_names = get_unique_names
+  print_combinations(unique_names)
 end
+
+main if __FILE__ == $0
